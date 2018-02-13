52c1614e604c6e5a60d3d5711ff73f3ce6646ce4
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-02-13 01:45:23 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,24 +1,20 @@
 class PigLatinizer
 
-  attr_reader :text
-
-  def piglatinize(word)
-    vowels = %w{a e i o u}
-    consonants = %w{b c d f g h j k l m n p q r s t v w x y z}
-    if vowels.include?(word[0].downcase)
-      word = word + "w"
-    elsif consonants.include?(word[0].downcase) && consonants.include?(word[1].downcase) && consonants.include?(word[2].downcase)
-      word = word.slice(3..-1) + word.slice(0,3)
-    elsif consonants.include?(word[0].downcase) && consonants.include?(word[1].downcase)
-      word = word.slice(2..-1) + word.slice(0,2)
-    else
-      word = word.slice(1..-1) + word.slice(0)
-    end
-    word + "ay"
+  def initialize
   end
 
+  def piglatinize(word)
+    if !(word.downcase)[0].scan(/[aeiou]/).empty?
+      word + 'way'
+    else
+      word[word.split(/[aeiou]/)[0].length..-1] + word.split(/[aeiou]/)[0] + 'ay'
+    end
+  end
 
-  def to_pig_latin(sentence)
-    sentence.split.collect { |word| piglatinize(word) }.join(" ")
+  def to_pig_latin(words)
+    words.split(' ').collect do |w|
+      word = PigLatinizer.new
+      word.piglatinize(w)
+    end.join(' ')
   end
 end
